0e187fd70823816c9a0d9cff8f954d4013b7e93f
tsikouliemilia <emilia.tsikouli@elev.ga.ntig.se>
2025-11-14 09:37:02 +0100

    påse

--- app.py
+++ app.py
@@ -22,5 +22,20 @@ while run:
         if query.lowe()in bag:
              print(f"hittade {query} i påsen")
     else:
-           print("Felaktigt komando, försök igen.")
+           query = input ("vad vill du söka efter:")
+
+class ANSI():
+    def background(code):
+        return "\33[{code}m".format(code=code)
+
+    def style_text(code):
+        return "\33[{code}m".format(code=code)
+
+    def color_text(code):
+        return "\33[{code}m".format(code=code)
+
+
+example_ansi = ANSI.background(
+    97) + ANSI.color_text(35) + ANSI.style_text(4) + " TESTE ANSI ESCAPE CODE"
+print(example_ansi)
 
